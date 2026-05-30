ed30430ea2f30ccda55076d92c080a9e4bba3146
Exploring-glitch <deysasha262@gmail.com>
2026-05-30 16:56:02 +0530

    fea(server): add delete post functionality and API endpoint

--- server/src/routes/postRoutes.js
+++ server/src/routes/postRoutes.js
@@ -1,10 +1,11 @@
 import express from "express";
 import { protect } from "../middleware/authMiddleware.js";
-import { createPost, getAllPosts, getMyPosts } from "../controllers/postController.js";
+import { createPost, getAllPosts, getMyPosts, deletePost } from "../controllers/postController.js";
 const postRouter = express.Router();
 
 postRouter.post("/", protect, createPost);
 postRouter.get("/", protect, getAllPosts);
 postRouter.get("/me", protect, getMyPosts);
+postRouter.delete("/:id", protect, deletePost);
 
 export default postRouter;
